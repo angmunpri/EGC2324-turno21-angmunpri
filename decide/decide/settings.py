47bb8ebcf463e24b47a6bb7260b227e2cea105c8
angmunpri <angmunpri@alum.us.es>
2023-11-30 09:55:02 +0100

    fix: manage.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -156,6 +156,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
